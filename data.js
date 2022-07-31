5d9250eb45c1c9b1b17785b7fcebdecd66022730
Holmes Lin <41220173+aiaurora@users.noreply.github.com>
2022-07-31 08:56:46 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,146 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-street-view-360-1",
+      "name": "Street View 360-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.526912314675032,
+        "pitch": 0.07295988523026509,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8192304924572493,
+          "pitch": 0.35120155592827196,
+          "rotation": 0,
+          "target": "1-street-view-360-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9766841562757627,
+          "pitch": -0.29505535801853355,
+          "title": "入口",
+          "text": "櫥窗內的袖珍模型"
+        }
+      ]
+    },
+    {
+      "id": "1-street-view-360-2",
+      "name": "Street View 360-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9367041629832356,
+          "pitch": 0.24747734611615257,
+          "rotation": 0,
+          "target": "2-street-view-360-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5696541756533069,
+          "pitch": -0.3905982326857895,
+          "title": "歐洲生活",
+          "text": "歐洲的傳統商家風景"
+        }
+      ]
+    },
+    {
+      "id": "2-street-view-360-3",
+      "name": "Street View 360-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.18945867419640017,
+        "pitch": 0.23337987857231468,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7509494133256611,
+          "pitch": 0.315626321281659,
+          "rotation": 0,
+          "target": "0-street-view-360-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3580820182859874,
+          "pitch": 0.017497612713363253,
+          "title": "大型櫥櫃",
+          "text": "許許多多比人高"
+        }
+      ]
+    }
+  ],
+  "name": "袖珍博物館導覽練習",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
